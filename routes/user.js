79cfb4057b648032979d60b12a5710f1474f323c
Ishanuj <ishanuj99@gmail.com>
2021-06-25 13:45:07 +0530

    Login token displayed on consoled

--- routes/user.js
+++ routes/user.js
@@ -67,6 +67,7 @@ router.post('/login', passport.authenticate('local', {session: false}), (req, re
   if(req.isAuthenticated()) {
       const {_id, username} = req.user;
       const token = signToken(_id);
+      console.log(token);
       res.cookie('access_token', token, {httpOnly: true, sameSite: true});
       res.status(200).json({isAuthenticated: true, user: {username}})
   }
