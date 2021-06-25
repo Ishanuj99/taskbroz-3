76fb1da3791e5218d3d50357092c8d4ace4533c7
Ishanuj <ishanuj99@gmail.com>
2021-06-25 14:05:27 +0530

    Console token

--- routes/user.js
+++ routes/user.js
@@ -69,7 +69,7 @@ router.post('/login', passport.authenticate('local', {session: false}), (req, re
       const token = signToken(_id);
       console.log(token);
       res.cookie('access_token', token, {httpOnly: true, sameSite: true});
-      res.status(200).json({isAuthenticated: true, user: {username}})
+      res.status(200).json({isAuthenticated: true, user: {username}, token:{token}})
   }
 });
 
