b914f92247d98b911cf3c8efce18601a057b1f60
Ishanuj <ishanuj99@gmail.com>
2021-06-29 14:10:59 +0530

    minor changes

--- app.js
+++ app.js
@@ -24,7 +24,9 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
 app.use((req,res,next) => {
-    res.header("Access-Control-Allow-Origin" ,'*');
+    //res.header("Access-Control-Allow-Origin" ,'*');
+    res.header("Access-Control-Allow-Origin" ,req.header('origin'));
+    res.setHeader('Access-Control-Allow-Credentials',true);
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept, Authentication"
